98e5eb28eead36711d385a76d213aa5e53b5aef2
Alejandro Morales <alejandromorjim@gmail.com>
2026-04-29 13:05:09 -0600

    solving issue of retrieval time changing views

--- app/metas/page.jsx
+++ app/metas/page.jsx
@@ -1,11 +1,9 @@
 import { getMetas } from '@/lib/actions/metas';
-import { getObjetivos } from '@/lib/actions/objetivos';
-import { getTareas } from '@/lib/actions/tareas';
 import ViewMetas from '@/components/views/ViewMetas';
 
 export default async function MetasPage() {
   const metas = await getMetas();
-  const objetivos = await getObjetivos();
-  const tareas = await getTareas();
+  const objetivos = metas.flatMap((m) => m.objetivos ?? []);
+  const tareas = objetivos.flatMap((o) => o.tareas ?? []);
   return <ViewMetas metas={metas} objetivos={objetivos} tareas={tareas} />;
 }
